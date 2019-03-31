c6049f4bf8f2af9c5d4769d3451791ebe6a504e8
ljcyu <ljcyu@163.com>
2019-03-31 12:25:12 +0800

    下载文件中文编码

--- src/main/java/controller/FileDown.java
+++ src/main/java/controller/FileDown.java
@@ -7,6 +7,7 @@ import org.apache.struts2.ServletActionContext;
 
 import javax.servlet.Servlet;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 public class FileDown extends ActionSupport {
   public InputStream getXml(){
@@ -21,6 +22,19 @@ public class FileDown extends ActionSupport {
   private String uploadPath;
   @Setter @Getter
   private String uploadFileName;
+  @Setter
+  private String cnFileName;
+
+  public String getCnFileName() {
+    cnFileName=uploadFileName;
+    try {
+      cnFileName=new String(uploadFileName.getBytes(),"iso-8859-1");
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+    return cnFileName;
+  }
+
   public InputStream getFileDown(){
     return ServletActionContext.getServletContext().getResourceAsStream(uploadPath);
   }
