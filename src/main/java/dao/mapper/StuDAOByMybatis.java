64dd3bf1324ad0006af124d194a0a77862cf2bdd
ljcyu <ljcyu@163.com>
2019-04-10 06:46:00 +0800

    准备其他方法，并测试

--- src/main/java/dao/mapper/StuDAOByMybatis.java
+++ src/main/java/dao/mapper/StuDAOByMybatis.java
@@ -1,16 +1,26 @@
 package dao.mapper;
 
 import domain.Stu;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface StuDAOByMybatis {
   @Select("select * from stu")
-   List<Stu> getAll();
+  List<Stu> getAll();
+
   @Select("select * from stu where id=#{id}")
   Stu findById(@Param("id") long id);
+
+  @Insert("insert into stu(username,java,math,os)" +
+      "values(#{stu.username},#{stu.java},#{stu.math},#{stu.os})")
+  @Options(useGeneratedKeys = true,keyProperty = "stu.id")
+  int add(@Param("stu")Stu stu);
+
+  @Update("update stu set username=#{stu.username},os=#{stu.os}," +
+      "java=#{stu.java},math=#{stu.math} where id=#{stu.id}")
+  int update(@Param("stu") Stu stu);
+  @Delete("delete from stu where id=#{id}")
+  int delById(@Param("id") long id);
 }
--- src/test/java/demo/StuDAOByMybatisTest.java
+++ src/test/java/demo/StuDAOByMybatisTest.java
@@ -11,6 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
@@ -22,5 +25,26 @@ public class StuDAOByMybatisTest {
   public void testGetAll(){
     List<Stu> stus=dao.getAll();
     log.debug("{}",stus);
+  }
+  @Test
+  public void testAdd()  {
+    Stu stu=new Stu();
+    stu.setUsername("test-1");
+    stu.setJava(90);stu.setMath(91);stu.setOs(92);
+    int row=dao.add(stu);
+    assertTrue(row>0);
+    log.debug("new id {}",stu.getId());
+  }
+
+  @Test
+  public void testUpdate(){
+   //update
+    Stu stu=dao.findById(8L);
+    String newName="java-2";
+    stu.setUsername(newName);
+    dao.update(stu);
+    //assertEquals
+    Stu newStu=dao.findById(8L);
+    assertEquals(newStu.getUsername(),newName);
   }
 }
