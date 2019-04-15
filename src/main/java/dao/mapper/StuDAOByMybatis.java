d7e51fc55e768853969541db59dcdc4882f4237f
ljcyu <ljcyu@163.com>
2019-04-15 11:50:59 +0800

    增加缓存注释

--- src/main/java/dao/mapper/StuDAOByMybatis.java
+++ src/main/java/dao/mapper/StuDAOByMybatis.java
@@ -2,25 +2,29 @@ package dao.mapper;
 
 import domain.Stu;
 import org.apache.ibatis.annotations.*;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
 @Mapper
 public interface StuDAOByMybatis {
+  @Cacheable("allStus")
   @Select("select * from stu")
   List<Stu> getAll();
-
+  @Cacheable("allStus")
   @Select("select * from stu where id=#{id}")
   Stu findById(@Param("id") long id);
-
+  @CacheEvict(value = "allStus",allEntries = true)
   @Insert("insert into stu(username,java,math,os)" +
       "values(#{stu.username},#{stu.java},#{stu.math},#{stu.os})")
   @Options(useGeneratedKeys = true,keyProperty = "stu.id")
   int add(@Param("stu")Stu stu);
-
+  @CacheEvict(value = "allStus",allEntries = true)
   @Update("update stu set username=#{stu.username},os=#{stu.os}," +
       "java=#{stu.java},math=#{stu.math} where id=#{stu.id}")
   int update(@Param("stu") Stu stu);
+  @CacheEvict(value = "allStus",allEntries = true)
   @Delete("delete from stu where id=#{id}")
   int delById(@Param("id") long id);
 }
