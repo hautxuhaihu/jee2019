169069af1e0b689aa0ae4ab0caa15c107ffe8329
ljcyu <ljcyu@163.com>
2019-04-17 14:41:34 +0800

    为分页查询指定不同的缓存名

--- src/main/java/dao/mapper/StuDAOByMybatis.java
+++ src/main/java/dao/mapper/StuDAOByMybatis.java
@@ -12,23 +12,30 @@ public interface StuDAOByMybatis {
   @Cacheable("allStus")
   @Select("select * from stu")
   List<Stu> getAll();
-  //@Cacheable(value = "pageStus")
+
+  @Cacheable(value = "allStus:page")
   @Select("select * from stu")
   List<Stu> getAllByPageNumSize(@Param("pageNum") int pageNum,@Param("pageSize")int pageSize);
 
   @Cacheable("allStus")
   @Select("select * from stu where id=#{id}")
   Stu findById(@Param("id") long id);
+
   @CacheEvict(value = "allStus",allEntries = true)
   @Insert("insert into stu(username,java,math,os)" +
       "values(#{stu.username},#{stu.java},#{stu.math},#{stu.os})")
   @Options(useGeneratedKeys = true,keyProperty = "stu.id")
   int add(@Param("stu")Stu stu);
+
   @CacheEvict(value = "allStus",allEntries = true)
   @Update("update stu set username=#{stu.username},os=#{stu.os}," +
       "java=#{stu.java},math=#{stu.math} where id=#{stu.id}")
   int update(@Param("stu") Stu stu);
+
   @CacheEvict(value = "allStus",allEntries = true)
   @Delete("delete from stu where id=#{id}")
   int delById(@Param("id") long id);
+
+  @Select("select count(*) from stu")
+  int total();
 }
